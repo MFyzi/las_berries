35c76f1cf4f0c79c567f47fa315cf1d4afc8b578
Muhammad Fyrooz Razi <108935358+MFyzi@users.noreply.github.com>
2022-10-04 13:55:17 +0530

    home page scroll sections

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,47 +1,52 @@
 // import React, { useEffect, useState } from 'react'
 // import Cards from '../components/home-cards/cards'
 // import {cakePic} from '../images/lasberry.jpg'
+import { useNavigate } from 'react-router-dom'
 import './home.css'
 // import useFireFetch from '../components/hooks/useFireFetch'
 
 function Home() {
-//  const {cakeData}= useFireFetch('cake')
-
+const navigate = useNavigate()
   return (
 
-    // <header className='home_container'>
-    //   <div className="home_card_container">
-    //         {/* {cakeData.map((item)=>
-    //         <Cards  
-    //         id = {item.id}
-    //         name = {item.name}
-    //         url ={item.url}
-    //         price = {item.price}
-    //         />
-    //         )} */}
-    //         <div className="section_hero">
-    //           handleLogin
-    //         </div>
-    //         <div className="section"></div>
-    //   </div>
-    // </header>  )
-
     <div className="home__container">
+        <p className='bar'></p>
       <header className='section'>
-      helloS
-        {/* <img src='./images/lasberry.jpg' alt=""  /> */}
-        <p></p>
+        <img src="./images/icon.jpg" alt="" />
       </header>
       <section className="section home__container_section-product">
-        
-        <h2>What we Provide</h2>
         <div className="product-cake">
+          <div className="product-cake-card">
+            <img src="./images/lasberry.jpg" alt="" />
+            <h3>Cakes</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maiores iure quos sint illum soluta expedita fuga atque accusamus totam temporibus doloribus dolorem rerum, asperiores ipsum ratione tempora. Incidunt, sequi!</p>
+            <button className='btn'onClick={()=>
+            navigate('/products/cakes')}>Shop</button>
+          </div>
         </div>
-        <div className="product-cupcake">cup</div>
-        <div className="product-jarcake">aj</div>
+        <div className="product-cupcake">
+        <div className="product-cupcake-card">
+            <img src="./images/lasberry.jpg" alt="" />
+            <h3>Cup Cakes</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maiores iure quos sint illum soluta expedita fuga atque accusamus totam temporibus doloribus dolorem rerum, asperiores ipsum ratione tempora. Incidunt, sequi!</p>
+            <button className='btn' onClick={()=>
+            navigate('/products/cupcakes')}>Shop</button>
+          </div>
+        </div>
+        <div className="product-jarcake">
+        <div className="product-jarcake-card">
+            <img src="./images/lasberry.jpg" alt="" />
+            <h3>JAr Cakes</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maiores iure quos sint illum soluta expedita fuga atque accusamus totam temporibus doloribus dolorem rerum, asperiores ipsum ratione tempora. Incidunt, sequi!</p>
+            <button className='btn'onClick={()=>
+            navigate('/products/jarcakes')}>Shop</button>
+          </div>
+        </div>
       </section>
       <section className="section home__container_section-about">
-        bye
+        <div className="about-card">
+          
+        </div>
       </section>
       <section className="section home__container_section-contact">
         now
