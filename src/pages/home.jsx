da5534b399c8aaa43a080a4d7d151d3d0e691e0d
Muhammad Fyrooz Razi <108935358+MFyzi@users.noreply.github.com>
2022-10-05 08:52:55 +0530

    home change

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,45 +12,50 @@ const navigate = useNavigate()
     <div className="home__container">
         <p className='bar'></p>
       <header className='section'>
-        <img src="./images/icon.jpg" alt="" />
+        <div className='filter'></div>
+        <div className="header-content">
+          <img src="./images/icon.jpg" alt="" />
+          <h1>Las Berries</h1>
+        </div>
+        {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae accusamus nihil quibusdam temporibus aut nam voluptates natus perferendis eius, repellendus obcaecati, consequuntur aliquam vitae veritatis.</p> */}
       </header>
       <section className="section home__container_section-product">
-        <div className="product-cake">
-          <div className="product-cake-card">
+      <div className="product product-cake">
+        <div className="product-card product-cake-card">
             <img src="./images/lasberry.jpg" alt="" />
             <h3>Cakes</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maiores iure quos sint illum soluta expedita fuga atque accusamus totam temporibus doloribus dolorem rerum, asperiores ipsum ratione tempora. Incidunt, sequi!</p>
-            <button className='btn'onClick={()=>
-            navigate('/products/cakes')}>Shop</button>
-          </div>
-        </div>
-        <div className="product-cupcake">
-        <div className="product-cupcake-card">
-            <img src="./images/lasberry.jpg" alt="" />
-            <h3>Cup Cakes</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maiores iure quos sint illum soluta expedita fuga atque accusamus totam temporibus doloribus dolorem rerum, asperiores ipsum ratione tempora. Incidunt, sequi!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maiores iure quos sint illum soluta expedita fuga atque Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sint officia vero facilis repellat esse.</p>
             <button className='btn' onClick={()=>
             navigate('/products/cupcakes')}>Shop</button>
           </div>
         </div>
-        <div className="product-jarcake">
-        <div className="product-jarcake-card">
+        <div className="product product-cupcake">
+        <div className="product-card product-cupcake-card">
+            <img src="./images/lasberry.jpg" alt="" />
+            <h3>Cup Cakes</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maiores iure quos sint illum soluta expedita fuga atque Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sint officia vero facilis repellat esse.</p>
+            <button className='btn' onClick={()=>
+            navigate('/products/cupcakes')}>Shop</button>
+          </div>
+        </div>
+        <div className="product product-jarcake">
+        <div className="product-card product-jarcake-card">
             <img src="./images/lasberry.jpg" alt="" />
             <h3>JAr Cakes</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maiores iure quos sint illum soluta expedita fuga atque accusamus totam temporibus doloribus dolorem rerum, asperiores ipsum ratione tempora. Incidunt, sequi!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maiores iure quos sint illum soluta expedita fuga atque Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugit, optio sunt dicta tempore eveniet?</p>
             <button className='btn'onClick={()=>
             navigate('/products/jarcakes')}>Shop</button>
           </div>
         </div>
       </section>
-      <section className="section home__container_section-about">
+      {/* <section className="section home__container_section-about">
         <div className="about-card">
           
         </div>
       </section>
       <section className="section home__container_section-contact">
         now
-      </section>
+      </section> */}
     </div>
 )}
 
